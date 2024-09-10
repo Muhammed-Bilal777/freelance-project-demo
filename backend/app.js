a8466a37d6d42ab3f5bdaaa5a4ec401797c3c5b8
Muhammed Bilal <mdbilaal777@gmail.com>
2024-09-11 03:22:37 +0530

    deploying

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 });
 const corsOptions = {
    
-  origin: 'http://localhost:3000' , //'https://freelance-project-demo-2.onrender.com',
+  origin: 'https://freelance-project-demo.onrender.com',
   // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   credentials: true,
   accessControlAllowCredentials: true,
